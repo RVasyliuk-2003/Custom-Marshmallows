e5dfe69bd1ff60b387ba8e2d34c0dde2de2dc217
Roman <vasylukroma77@gmail.com>
2026-08-07 15:28:55 +0200

    feat: add Header component with styles

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import "./header.css";
+import Logo from "./../../assets/logo.png";
+import Menu from "./../../assets/menu.png";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="container navContainer">
+        <div className="NavLogoBox">
+          <img src={Logo} alt="Logo" />
+          <h4>Зефірка</h4>
+        </div>
+
+        <img className="imgMenu" src={Menu} alt="Menu" />
+      </nav>
+      ;
+    </>
+  );
+};
+
+export default Header;
