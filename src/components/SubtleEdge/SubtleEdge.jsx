353eb284d979e139e4e93dcd708f5d186ae127a4
Roman <vasylukroma77@gmail.com>
2026-08-08 21:33:09 +0200

    feat: add SubtleEdge SVG component for section dividers

---
+++ src/components/SubtleEdge/SubtleEdge.jsx
@@ -0,0 +1,12 @@
+import "./subtleEdge.css";
+const SubtleEdge = ({ flip = false }) => (
+  <svg
+    className={`subtleEdge ${flip ? "flip" : ""}`}
+    viewBox="0 0 375 16"
+    preserveAspectRatio="none"
+  >
+    <path d="M0,8 Q20,0 40,8 Q60,16 80,8 Q100,0 120,8 Q140,16 160,8 Q180,0 200,8 Q220,16 240,8 Q260,0 280,8 Q300,16 320,8 Q340,0 360,8 Q375,4 375,8 L375,16 L0,16 Z" />
+  </svg>
+);
+
+export default SubtleEdge;
