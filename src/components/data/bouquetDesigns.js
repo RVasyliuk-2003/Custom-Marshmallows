640902a35bd51bd25543a772973ddb36e84d7b65
Roman <vasylukroma77@gmail.com>
2026-08-29 11:42:30 +0200

    refactor: move static data arrays to a dedicated data folder

---
+++ src/components/data/bouquetDesigns.js
@@ -0,0 +1,19 @@
+const bouquetDesigns = [
+  {
+    id: 1,
+    name: "Ніжна класика",
+    type: "bouquet",
+    photo: "klasyka.jpg",
+    availableSizes: [10, 16, 18, 20, 25, 30],
+  },
+
+  {
+    id: 2,
+    name: "Проста завитка",
+    type: "curl",
+    photo: "curl.jpg",
+    availableSizes: null,
+  },
+];
+
+export default bouquetDesigns;
