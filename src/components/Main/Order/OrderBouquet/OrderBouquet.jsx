189912f9f14824351ce533f308782cf084fe5173
Roman <vasylukroma77@gmail.com>
2026-09-03 11:55:51 +0200

    chore: scaffold OrderBouquet component

---
+++ src/components/Main/Order/OrderBouquet/OrderBouquet.jsx
@@ -0,0 +1,11 @@
+import style from "./OrderBouquet.module.css";
+
+const OrderBouquet = () => {
+  return (
+    <section>
+      <div className="container"></div>
+    </section>
+  );
+};
+
+export default OrderBouquet;
