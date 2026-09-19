834fa55e5a69fc7fd66d9204f88ce9fa9fa85c84
Roman <vasylukroma77@gmail.com>
2026-09-19 16:04:20 +0200

    feat(contact): integrate FadeUp animations for contact cards and submit button

--- src/components/Main/Contact/Contact.jsx
+++ src/components/Main/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import style from "./contact.module.css";
+import { FadeUp } from "../../FadeUp";
 import { useState } from "react";
 
 const Contact = () => {
@@ -53,44 +54,64 @@ const Contact = () => {
     <section>
       <div className="container">
         <div className={style.mainContainer}>
-          <a
+          <FadeUp
+            as="a"
             href="https://instagram.com/olenavasyliuk69/"
             target="_blank"
             rel="noopener noreferrer"
             className={style.contactBox}
+            duration={0.5}
+            delay={0.1}
+            yOffset={20}
           >
             <div className={style.icon}>📸</div>
             <div className={style.contactLink}>
               <b>Instagram</b>
               <span>@zefirka.ua</span>
             </div>
-          </a>
-          <a
+          </FadeUp>
+          <FadeUp
+            as="a"
             href="https://t.me/ZefirkaOlena"
             target="_blank"
             rel="noopener noreferrer"
             className={style.contactBox}
+            duration={0.5}
+            delay={0.2}
+            yOffset={20}
           >
             <div className={style.icon}>💬</div>
             <div className={style.contactLink}>
               <b>Telegram</b>
               <span>@ZefirkaOlena</span>
             </div>
-          </a>
-          <a className={style.contactBox} href="tel:+380 63 775 13 84">
+          </FadeUp>
+          <FadeUp
+            as="a"
+            className={style.contactBox}
+            href="tel:+380 63 775 13 84"
+            duration={0.5}
+            delay={0.3}
+            yOffset={20}
+          >
             <div className={style.icon}>📞</div>
             <div className={style.contactLink}>
               <b>Телефон</b>
               <span>+380 63 775 13 84</span>
             </div>
-          </a>
-          <div className={style.contactBox}>
+          </FadeUp>
+          <FadeUp
+            className={style.contactBox}
+            duration={0.5}
+            delay={0.4}
+            yOffset={20}
+          >
             <div className={style.icon}>🕘</div>
             <div className={style.contactLink}>
               <b>Графік</b>
               <span>щодня 9:00–20:00</span>
             </div>
-          </div>
+          </FadeUp>
           <div className={style.mapBlock}>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16961.448509463036!2d28.81780026877435!3d49.71453448640261!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472ce0090a270309%3A0x3de5384b6bbd3b28!2sHrosh!5e0!3m2!1sru!2spl!4v1787399224292!5m2!1sru!2spl"
@@ -162,13 +183,17 @@ const Contact = () => {
             </p>
           ) : null}
 
-          <button
+          <FadeUp
+            as="button"
             onClick={() => resultContactForm()}
             style={{ marginTop: "16px" }}
             className="btn btn-primary"
+            duration={0.7}
+            delay={0.3}
+            yOffset={10}
           >
             Надіслати
-          </button>
+          </FadeUp>
         </div>
       </div>
     </section>
